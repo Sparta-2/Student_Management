e47d3ed58211a9d2a811cbbb6a30eed0f78810f8
Changmin Kim <kcm0411@gmail.com>
2024-05-03 20:36:55 +0900

    add file test

---
+++ Student_Management/src/Score.java
@@ -0,0 +1,12 @@
+public class Score {
+
+    // 점수, 등급
+    private int score;
+    private String grade;
+
+//    public String getGrade(){
+//
+//    }
+
+
+}
---
+++ Student_Management/src/ScoreEdit.java
@@ -0,0 +1,2 @@
+public class ScoreEdit {
+}
